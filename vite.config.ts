b350d127ab9fe65cd78085b830a77cee34abe232
zangbuqing_ext <zangbuqing@richinfo.cn>
2024-04-19 10:50:35 +0800

    111111

--- vite.config.ts
+++ vite.config.ts
@@ -17,18 +17,17 @@ export default defineConfig({
   },
 
   server: {
-    port:4000,//设置服务启动端口号，是一个可选项，不要设置为本机的端口号，可能会发生冲突
-    open:true,//是否自动打开浏览器，可选项
-    cors:true,//允许跨域。
+    port: 4000, //设置服务启动端口号，是一个可选项，不要设置为本机的端口号，可能会发生冲突
+    open: true, //是否自动打开浏览器，可选项
+    cors: true, //允许跨域。
     // 设置代理
     proxy: {
       // 将请求代理到另一个服务器
-      '/api': {
-        target: 'http://cn-lmg-bgp-xhfrp.xhzdim.top:38362',//这是你要跨域请求的地址前缀
-        changeOrigin: true,//开启跨域
-        rewrite: path => path.replace(/^\/api/, ''),//去除前缀api
-      }
-    }
-
-  }
+      "/api": {
+        target: "http://cn-lmg-bgp-xhfrp.xhzdim.top:38362", //这是你要跨域请求的地址前缀
+        changeOrigin: true, //开启跨域
+        rewrite: (path) => path.replace(/^\/api/, ""), //去除前缀api
+      },
+    },
+  },
 });
